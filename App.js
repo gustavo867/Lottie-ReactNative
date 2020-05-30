062a3c8cfe1a2693181bdc97557fe564d83b234f
gustavo867 <tartatavo3@gmail.com>
2020-05-30 17:57:38 -0300

    Testing Lottie

--- App.js
+++ App.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Button, Alert, Text } from 'react-native';
+import Lottie from 'lottie-react-native';
+
+import rocket from './rocket.json';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontWeight: "bold", fontSize: 20}}>Testing Lottie</Text>
+      <Lottie resizeMode= "contain" autoSize source={rocket} autoPlay loop/>
+      <Button 
+      onPress={() => {
+        Alert.alert("Testing Lottie")
+      }}
+      title="Rocket Lottie"
+      color="#841584"
+      accessibilityLabel="Learn more about this purple button"
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ README.md
@@ -0,0 +1,13 @@
+## Testing Lottie
+-[x] Use lottie in expo
+-[x] Create a button with a alert 
+
+## Pictures 
+
+![image](https://user-images.githubusercontent.com/63013756/83338854-9665ae00-a29e-11ea-9d3a-78c28957b034.png)
+
+
+## Technologies
+- Expo
+- React Native
+- Lottie React Native
